ccbda63b74ba2bba644a7ea43831a2f3dd42e5a9
ylm <yingleiming@outlook.com>
2021-03-02 16:22:28 +0800

    init

---
+++ pages/home/home.js
@@ -0,0 +1,79 @@
+// pages/home/home.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+	indicatorDots:true,
+	imgUrl:[
+		'./../../images/swiper/IMG_1839.JPG', 
+		'./../../images/swiper/IMG_1840.JPG', 
+		'./../../images/swiper/IMG_1841.JPG', 
+	]	
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+	wx.getLocation({
+		type: 'wgs84',
+		success (res) {
+		  const latitude = res.latitude
+		  const longitude = res.longitude
+		  const speed = res.speed
+		  const accuracy = res.accuracy
+		}
+	   })
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
